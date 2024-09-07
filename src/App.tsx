07417721ceeba0dbe846c20a3f9d09df36986ae0
nod1rDev <nodirbeknegmatov2008@gmail.com>
2024-09-07 22:31:27 +0500

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignIn from "./Components/SignIn";
 import SignUp from "./Components/SignUp";
 import Home from "./Components/Home";
@@ -7,28 +7,29 @@ import Protected from "./Components/Protected";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Protected>
+        <Home />
+      </Protected>
+    ),
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
+]);
 
 const App: React.FC = () => {
-  
-
- 
-
   return (
     <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Protected>
-                <Home />
-              </Protected>
-            }
-          />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-        </Routes>
-      </Router>
+      <RouterProvider router={router} />
     </Provider>
   );
 };
